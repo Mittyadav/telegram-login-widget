a2427dc879c6fe5f59b960b0eddf18ac0b68af3b
@mitt_ydv <146659530+Mittyadav@users.noreply.github.com>
2024-11-28 01:07:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const crypto = require('crypto');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
